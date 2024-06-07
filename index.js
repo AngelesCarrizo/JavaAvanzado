28fa53393134573d8072c7eb4289ee65c7dddeb3
Angeles Carrizo <carrizoangeles91@gmail.com>
2024-06-07 15:57:06 -0300

    add index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express  from 'express'
 import { readFile, writeFile } from 'fs/promises'
-import userRouter from './routes/usuarios.routes.js'
-import prodRouter from './routes/productos.routes.js'
-import venRouter from './routes/productos.routes.js'
+import userRouter from './backend/routes/usuarios.routes.js'
+import prodRouter from './backend/routes/productos.routes.js'
+import venRouter from './backend/routes/ventas.routes.js'
 
 const app = express()
 const port = 3000
@@ -11,8 +11,9 @@ app.use(express.json())
 app.use('/user', userRouter)
 app.use('/prod', prodRouter)
 app.use('/ven', venRouter)
-app.use(express.static('./client'))
-
+app.use('/imagenes', express.static('./frontend/imagenes'));
+app.use(express.static('./frontend/src/pages'))
+app.use(express.static('./frontend/src/styles'))
 app.listen(port,()=>{
     console.log(`Servidor levantado en el puerto ${port}`)
 })
